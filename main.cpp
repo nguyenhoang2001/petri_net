32fc9458d79a4c22c8672b31a227b9ee15f08fbb
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-11-08 11:46:10 +0700

    done for petri-net

--- main.cpp
+++ main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 #include <vector>
-// #define FREE 0
-// #define DOCU 2
-// #define BUSY 1
 
-// #define DONE 2
-// #define WAIT 0
-// #define INSIDE 1
-// #define COUNT 3
-
-// #define WAITING 0
-// #define TREATING 1
-// #define GIVE_DOCUMENT 2
-// #define DONE_TREATING 3
-// #define FREEING 4
-
-// #define MAX_INPUT 10
+#define MAX_INPUT 10
 
 using namespace std;
 
@@ -24,155 +10,102 @@ struct transition
     vector<int> input;
     vector<int> output;
 };
-// int specialists[3] = {0, 0, 0};
-// int patients[4] = {0, 0, 0, 0};
-// int patient_and_specialists[5] = {0, 0, 0, 0, 0};
 
-// void Update_Transitions(transition *arr, int size)
-// {
-//     for (int i = 0; i < size; i++)
-//     {
-//         bool able = true;
-//         for (int j = 0; j < arr[i].input.size(); j++)
-//         {
-//             if (patient_and_specialists[arr[i].input[j]] <= 0)
-//                 able = false;
-//         }
-//         if (able)
-//         {
-//             for (int j = 0; j < arr[i].input.size(); j++)
-//             {
-//                 patient_and_specialists[arr[i].input[j]] -= 1;
-//             }
-//             for (int j = 0; j < arr[i].output.size(); j++)
-//             {
-//                 patient_and_specialists[arr[i].output[j]] += 1;
-//             }
-//         }
-//         if (i == 0)
-//         {
-//             cout << "Start: [ ";
-//             for (int j = 0; j < 3; j++)
-//             {
-//                 if (j == 0)
-//                 {
-//                     cout << specialists[j];
-//                 }
-//                 else
-//                 {
-//                     cout << ", " << specialists[j];
-//                 }
-//             }
-//             cout << " ]" << endl;
-//         }
-//         else if (i == 1)
-//         {
-//             cout << "Change: [ ";
-//             for (int j = 0; j < 3; j++)
-//             {
-//                 if (j == 0)
-//                 {
-//                     cout << specialists[j];
-//                 }
-//                 else
-//                 {
-//                     cout << ", " << specialists[j];
-//                 }
-//             }
-//             cout << " ]" << endl;
-//         }
-//         else
-//         {
-//             cout << "End: [ ";
-//             for (int j = 0; j < 3; j++)
-//             {
-//                 if (j == 0)
-//                 {
-//                     cout << specialists[j];
-//                 }
-//                 else
-//                 {
-//                     cout << ", " << specialists[j];
-//                 }
-//             }
-//             cout << " ]" << endl;
-//         }
-//     }
-// }
+void print_reachable_marking(int *places, int size_places, transition *trans, int size_trans)
+{
+    int save_places[size_places];
+    for (int i = 0; i < size_places; i++)
+    {
+        save_places[i] = places[i];
+    }
+    for (int i = 0; i < size_trans; i++)
+    {
+        bool able = true;
+        for (int j = 0; j < trans[i].input.size(); j++)
+        {
+            if (save_places[trans[i].input[j]] == 0)
+                able = false;
+        }
+        if (able)
+        {
+            for (int j = 0; j < trans[i].input.size(); j++)
+            {
+                save_places[trans[i].input[j]] -= 1;
+            }
+            for (int j = 0; j < trans[i].output.size(); j++)
+            {
+                save_places[trans[i].output[j]] = 1;
+            }
+        }
+        cout << "Reachable marking from firing transition[" << i << "]: [ ";
+        for (int j = 0; j < size_places; j++)
+        {
+            if (j == 0)
+                cout << save_places[j];
+            else
+            {
+                cout << ", " << save_places[j];
+            }
+        }
+        cout << " ]" << endl;
+        for (int j = 0; j < size_places; j++)
+            save_places[j] = places[j];
+    }
+}
 
 int main()
 {
-    // transition spec_and_patient[4];
-    // for (int i = 0; i < 4; i++)
-    // {
-    //     if (i == 0)
-    //     {
-    //         spec_and_patient[i].input.push_back(WAITING);
-    //         spec_and_patient[i].input.push_back(FREEING);
-
-    //         spec_and_patient[i].output.push_back(BUSY);
-    //     }
-    //     else if (i == 1)
-    //     {
-    //         spec_and_patient[i].input.push_back(TREATING);
-    //         spec_and_patient[i].output.push_back(GIVE_DOCUMENT);
-    //     }
-    //     else
-    //     {
-    //         spec_and_patient[i].input.push_back(GIVE_DOCUMENT);
-    //         spec_and_patient[i].output.push_back(FREEING);
-    //         spec_and_patient[i].output.push_back(DONE_TREATING);
-    //     }
-    // }
-    // cout << "Input for the number of tokens for M0: ";
-    // int num = 0;
-    // cin >> num;
-    // patient_and_specialists[WAITING] = num;
     int num_of_places = 0;
     cout << "number of places: ";
     cin >> num_of_places;
     int num_of_trans = 0;
     cout << "number of transitions: ";
     cin >> num_of_trans;
-
-    transition num_trans[num_of_trans];
+    cout << "Press -1 if want to stop" << endl;
+    transition trans[num_of_trans];
     for (int i = 0; i < num_of_trans; i++)
     {
         int index_input;
-        cout << "input of transition: " << i << ": ";
+        cout << "input of transition[ " << i << " ]: ";
         while (true)
         {
             cin >> index_input;
             if (index_input == -1)
                 break;
-            num_trans[i].input.push_back(index_input);
+            trans[i].input.push_back(index_input);
         }
         int output_index;
-        cout << endl;
-        cout << "outputs of transition: " << i << ": ";
+        cout << "outputs of transition[ " << i << " ]: ";
         while (true)
         {
             cin >> output_index;
             if (output_index == -1)
                 break;
-            num_trans[i].output.push_back(output_index);
+            trans[i].output.push_back(output_index);
         }
     }
-    cout << endl;
     int places[num_of_places];
     for (int i = 0; i < num_of_places; i++)
     {
-        cout << "number of tokens in place: " << i << ": ";
+        cout << "number of tokens in place[ " << i << " ]: ";
         int num_of_tokens = 0;
         cin >> num_of_tokens;
         places[i] = num_of_tokens;
     }
-    cout << "the initial state: ";
-    int initial_state = 0;
-    cin >> initial_state;
-
+    // cout << "the initial state: ";
+    // int initial_state = 0;
+    // cin >> initial_state;
+    cout << "Initial markings: [ ";
     for (int i = 0; i < num_of_places; i++)
-        cout << places[i] << " ";
-
+    {
+        if (i == 0)
+        {
+            cout << places[i];
+        }
+        else
+            cout << ", " << places[i];
+    }
+    cout << " ]" << endl;
+    print_reachable_marking(places, num_of_places, trans, num_of_trans);
     return 0;
 }
